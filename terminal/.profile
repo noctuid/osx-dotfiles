66566f25f60737e391ce7c1194224be03d9e5f51
Fox Kiester <wyvern@posteo.net>
2021-09-09 16:23:38 -0400

    Update shell config files
    - Unset ALTERNATE_EDITOR
    - Add pyenv configuration
    - Add restartkarabiner and vm/vmb aliases

--- terminal/.profile
+++ terminal/.profile
@@ -1,2 +1,8 @@
 export EDITOR='emacsclient -a "" -c'
+export ALTERNATE_EDITOR=
 export EA_EDITOR='emacsclient -a "" -c'
+
+# set up pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
--- terminal/.zsh/local.zsh
+++ terminal/.zsh/local.zsh
@@ -1,2 +1,15 @@
 # shellcheck disable=1090
 source ~/dotfiles/scripts/aliases/stow.sh
+
+alias vm='~/src/jabberwocky/vertexhmi/version-manager/version-manager'
+alias vmb='~/src/jabberwocky/vertexb/version-manager/version-manager'
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+
+restartkarabiner() {
+    service=org.pqrs.karabiner.karabiner_console_user_server
+    launchctl stop "$service" && launchctl start "$service"
+}
